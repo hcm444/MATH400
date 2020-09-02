847bf46a687cd1d0ed2becacbfd70e0165c3f1e5
Henry Meier <32826270+hcm444@users.noreply.github.com>
2020-09-02 00:15:28 -0700

    Update funcmin.cpp

--- funcmin.cpp
+++ funcmin.cpp
@@ -1,23 +1,23 @@
-#include <stdio.h>
-#include <math.h>
-int main(){
-    double A = -2;
-    double dx;
-    dx = (2 - A) / 10;
-    int mark;
-    mark = 1;
-    double X;
-    X = A + mark * dx;
-    while (10e-6 <= fabs(A - X)) {
-        if (sin(X) <= sin(A)) {}
-        else {
+#include <stdio.h> //io
+#include <math.h> //math
+int main(){ //main funtion
+    double A = -2; //define A double
+    double dx; //define derivative
+    dx = (2 - A) / 10; //distance
+    int mark; //mark is for "-" aka direction of integration
+    mark = 1; //set mark to 1
+    double X; //double for X value
+    X = A + mark * dx; //limit
+    while (10e-6 <= fabs(A - X)) { //tolerance value
+        if (sin(X) <= sin(A)) {} //if less then
+        else { //dx = dx/10 or "/=" cuts up the limit again
             dx /= 10; //divide by ten again
             mark = -mark; //reverse
         }
-        A = X;
+        A = X; //set A to X
         X = A + mark * dx;
     }
-    auto xmin = X;
+    auto xmin = X; //set min function
     printf("xmin = %lf\n", xmin);
     printf("f(%lf) = %lf\n",xmin, sin(xmin));
 }
